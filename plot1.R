01c40476e7034e6378992e2804f1bc2451a2a1bd
Farkyster21 <chirose21@gmail.com>
2014-11-04 22:14:47 -0800

    Updated Comments

--- plot1.R
+++ plot1.R
@@ -10,10 +10,12 @@ electric$Date <- as.Date(electric$Date, "%d/%m/%Y")
 ##Dates from: 2007-02-01 and 2007-02-02
 data <- subset(electric, electric$Date %in% c(as.Date("2007-02-01"), as.Date("2007-02-02")))
 
-##For plot 1
+##For plot 1: Specifying color of histogram and labelling
 data$Global_active_power <- as.numeric(data$Global_active_power)
 hist(data$Global_active_power, col = "red", xlab = "Global Active Power (kilowatts)", main = "Global Active Power")
 axis(1, at = c(0,200,400,600,800,1000,1200))
+
+##Writing to a PNG file and closing the device
 dev.copy(png, file = "plot1.png", width = 480, height = 480)
 dev.off(dev.prev())
 
