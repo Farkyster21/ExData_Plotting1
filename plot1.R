bd3bd09b7510fc58d2a588286a9b9603657a2ab1
Farkyster21 <chirose21@gmail.com>
2014-11-04 22:08:57 -0800

    R coding file for plot 1

---
+++ plot1.R
@@ -0,0 +1,20 @@
+
+##Granted that you have a data set saved in your working directory as
+##"household_power_consumption.txt"
+
+##Reading in the data
+electric <- read.csv("household_power_consumption.txt", sep = ";", stringsAsFactors = FALSE)
+electric$Date <- as.Date(electric$Date, "%d/%m/%Y")
+
+##Subsetting the data to specific time frames
+##Dates from: 2007-02-01 and 2007-02-02
+data <- subset(electric, electric$Date %in% c(as.Date("2007-02-01"), as.Date("2007-02-02")))
+
+##For plot 1
+data$Global_active_power <- as.numeric(data$Global_active_power)
+hist(data$Global_active_power, col = "red", xlab = "Global Active Power (kilowatts)", main = "Global Active Power")
+axis(1, at = c(0,200,400,600,800,1000,1200))
+dev.copy(png, file = "plot1.png", width = 480, height = 480)
+dev.off(dev.prev())
+
+
